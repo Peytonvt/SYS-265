5414cea1d29a25f4c01506cf77a632c6c4bd2057
Peytonvt <peyton.pajak-leavy@mymail.champlain.edu>
2024-04-14 22:15:46 -0400

    Added a secure-ssh.sh file. We will build a script using this file.

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author peyton
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes root ability to ssh in
+echo "All YOUR CODE GOES HERE"

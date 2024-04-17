f9f57fcbea6402bf014ac2808dc8be08830f90bf
Peytonvt <peyton.pajak-leavy@mymail.champlain.edu>
2024-04-17 19:12:30 +0000

    Fixed script again....

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,13 +1,13 @@
 #secure-ssh.sh
-#author peyton
+#author peytonpajak
 #creates a new ssh user using $1 parameter
-#adds a public key from the local repo or curled from the remote repo
-#removes root ability to ssh in
-echo "All YOUR CODE GOES HERE"
+#adds a public key from the local repo or curled from the remote open
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
 
-sudo useradd -m -d /home/testUser3 -s /bin/bash testUser3
-sudo mkdir /home/testUser3/.ssh
-cp /home/peyton/SYS-265/linux/public-keys/id_rsa.pub /home/testUser3/.ssh/authorized_keys
-chmod 600 /home/testUser3/.ssh/authorized_keys
-chmod 700 /home/testUser3/.ssh/
-chown -R testUser3:testUser3 /home/testUser3/.ssh
+useradd -m -d /home/"$1" -s /bin/bash "$1"
+sudo mkdir /home/"$1"/.ssh
+cp /home/peyton/SYS-265/linux/public-keys/id_rsa.pub /home/"$1"/.ssh/authorized_keys
+chmod 700 /home/"$1"/.ssh
+chmod 600 /home/"$1"/.ssh/authorized_keys
+chown -R "$1":"$1" /home/"$1"/.ssh

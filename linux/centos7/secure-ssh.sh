24b07ba2b3642b723fec7bb8dbb63086003d7135
Peytonvt <peyton.pajak-leavy@mymail.champlain.edu>
2024-04-17 14:11:00 -0400

    Modified secure-ssh.sh file

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,3 +4,10 @@
 #adds a public key from the local repo or curled from the remote repo
 #removes root ability to ssh in
 echo "All YOUR CODE GOES HERE"
+
+sudo useradd -m -d /home/testUser -s /bin/bash testUser
+sudo mkdir /home/testUser/.ssh
+sudo cp /home/peyton/SYS265/linux/public-keys/id_rsa.pub /home/testUser/.ssh/authorized_keys
+sudo chmod 600 /home/testUser/.ssh/authorized_keys
+sudo chmod 700 /home/testUser/.ssh/
+sudo chown -R testUser:testUser /home/testUser/.ssh

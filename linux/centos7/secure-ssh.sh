3cb199e7214a681c3d445c35cb06d378fd42c08c
Peytonvt <peyton.pajak-leavy@mymail.champlain.edu>
2024-04-17 14:26:25 -0400

    Changed .ssh file again

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -5,9 +5,9 @@
 #removes root ability to ssh in
 echo "All YOUR CODE GOES HERE"
 
-sudo useradd -m -d /home/testUser2 -s /bin/bash testUser2
-sudo mkdir /home/testUser2/.ssh
-cp /home/peyton/SYS-265/linux/public-keys/id_rsa.pub /home/testUser2/.ssh/authorized_keys
-chmod 600 /home/testUser2/.ssh/authorized_keys
-chmod 700 /home/testUser2/.ssh/
-chown -R testUser2:testUser2 /home/testUser2/.ssh
+sudo useradd -m -d /home/testUser3 -s /bin/bash testUser3
+sudo mkdir /home/testUser3/.ssh
+cp /home/peyton/SYS-265/linux/public-keys/id_rsa.pub /home/testUser3/.ssh/authorized_keys
+chmod 600 /home/testUser3/.ssh/authorized_keys
+chmod 700 /home/testUser3/.ssh/
+chown -R testUser3:testUser3 /home/testUser3/.ssh
